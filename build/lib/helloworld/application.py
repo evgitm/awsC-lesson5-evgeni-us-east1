7302d386b1656cd426ffe5612f2eca38b9f235ca
EC2 Default User <ec2-user@ip-172-31-20-74.ec2.internal>
2021-06-08 16:02:23 +0000

    added CORS support2

--- build/lib/helloworld/application.py
+++ build/lib/helloworld/application.py
@@ -1,9 +1,15 @@
 #!flask/bin/python
 import json
+import requests
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+from flask_cors import CORS
 
 application = Flask(__name__)
+
+#כfor CORS support (requirements update  + app import CORS required)
+CORS(application, resources={r"/*": {"origins": "*"}}) 
+
 
 @application.route('/', methods=['GET'])
 def get():
@@ -12,6 +18,56 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
 
+
+
+currency_rate = {
+    'usd' : 3.3,
+    'pound' : 4.5,
+    'euro' : 4.8
+}
+
+
+@application.route('/calc/currency/<string:currency>', methods=['GET'])
+def post_currency(currency):
+    res = currency_rate.get(currency, 0.00) 
+    return Response(json.dumps({currency: res}), mimetype='application/json', status=200)
+
+
+
+@application.route('/calc/bit', methods=['GET'])
+def post_currency_bit():
+    return Response(json.dumps(get_bitcoin_index()), mimetype='application/json', status=200)
+
+
+
+def get_bitcoin_index():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url).json()['bpi']['USD']
+    return response
+
+
+# return generic data
+@application.route('/get_generic', methods=['GET'])
+def get_generic_data():
+    return Response(json.dumps(generic_data), mimetype='application/json', status=200)
+
+#generic data
+generic_data = [
+    {
+    "id":1,
+    "title": "wtf",
+    "body": "good will"
+    },
+    {
+    "id":2,
+    "title": "wtf2",
+    "body": "good will2"
+    }
+   ]
+   
+   
 if __name__ == '__main__':
     flaskrun(application)
+
