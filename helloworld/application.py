99e0c66bad91ed963a19d949a510c89f3bcd483c
EC2 Default User <ec2-user@ip-172-31-20-74.ec2.internal>
2021-06-08 14:26:23 +0000

    with bitcoin

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 import json
+import requests
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 
@@ -12,6 +13,30 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
+
+@application.route('/calc/currency/<string:currency>', methods=['GET'])
+def post_currency(currency):
+    res = currency_rate.get(currency, 0.00) 
+    return Response(json.dumps({currency: res}), mimetype='application/json', status=200)
+
+
+currency_rate = {
+    'usd' : 3.3,
+    'pound' : 4.5,
+    'euro' : 4.8
+}
+
+
+@application.route('/calc/bit', methods=['GET'])
+def post_currency_bit():
+    return Response(json.dumps(get_bitcoin_index()), mimetype='application/json', status=200)
+
+
+def get_bitcoin_index():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url).json()['bpi']['USD']
+    return response
 
 if __name__ == '__main__':
     flaskrun(application)
